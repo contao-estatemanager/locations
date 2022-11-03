608fec16b6be033c6f273082d783012f5eb2a39e
Sebastian Zoglowek <sebastian@oveleon.de>
2022-11-03 18:26:12 +0100

    [Bugfix] Fix undefined array keys

--- src/Resources/contao/dca/tl_department.php
+++ src/Resources/contao/dca/tl_department.php
@@ -63,7 +63,7 @@ $GLOBALS['TL_DCA']['tl_department'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_department']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.svg',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
                 'button_callback' => ['tl_department', 'deleteDepartment'],
             ],
             'show' => [
